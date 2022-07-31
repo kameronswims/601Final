8136d6e893da76b9bdec4d778ae50012207d1e38
kameronswims <106997789+kameronswims@users.noreply.github.com>
2022-07-31 10:48:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {Pokedex} from 'pokeapi-js-wrapper';
+import React, {useState, useEffect} from 'react';
+import PokemonList from "./PokemonList";
+import PokemonInfo from "./PokemonInfo";
+import PokedexSelector from "./PokedexSelector.jsx"
 
 function App() {
+  const [hasError, setErrors] = useState(null);
+  const [pokedexs, setPokedexs] = useState([]);
+  const [pokemonSelected, setPokemonSelected] = useState(null);
+  const [pokedexSelected, setPokedexSelected] = useState(null);
+  const P = new Pokedex.Pokedex()
+  
+  console.log("Pokedex 1: "+pokedexSelected);
+  console.log("Pokemon 1: "+pokemonSelected);
+
+  const choosePokedex = selection => setPokedexSelected(selection);
+  const choosePokemon = selection => setPokemonSelected(selection);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await new P.resource("/api/v2/pokedex");
+        const results = response.results;
+        setPokedexs(results);
+      } catch (e) {
+        setErrors("An error has occured: "+e.message);
+      }
+    }
+    fetchData();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <>
+      {hasError? 'An error has occured': null}
+      </>
+      {!pokedexSelected ?
+      <PokedexSelector pokedexs = {pokedexs} onSelection={choosePokedex} />
+    : null}
+    <hr />
+    {pokedexSelected && PokemonList && pokemonSelected? <PokemonInfo pokemonSelected={pokemonSelected}/>: null}
     </div>
   );
 }
